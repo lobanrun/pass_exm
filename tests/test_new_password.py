4903ef6523b46fea4f6f5219ad804110a078de15
Sariells <wwqkere@mail.ru>
2025-05-18 14:47:49 +0500

    test new password update

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -7,7 +7,19 @@ def test_password_characters():
     password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
     for char in password:
         assert char in valid_characters
+        
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной"""
+    password_length = 12
+    password = generate_password(password_length)
+    assert len(password) == password_length, f"Длина пароля {len(password)} не совпадает с ожидаемой {password_length}"
 
+
+
+def test_empty_password():
+    """Тест, что при генерации пароля длины 0 возвращается пустая строка"""
+    password = generate_password(0)
+    assert password == "", "Пароль не пустой"
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
