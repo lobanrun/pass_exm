91bdbad6ca67c417425a0c6766e7cd2aeba73451
LordDomon <lorddomon001@gmail.com>
2025-05-18 12:30:06 +0200

    �test_new password

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -4,7 +4,7 @@ from password.new_password import generate_password
 def test_password_characters():
     """Тест, что при генерации используются только допустимые символы"""
     valid_characters = string.ascii_letters + string.digits + string.punctuation
-    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    password = generate_password(101)  # Генерируем длинный пароль для более надежной проверки
     for char in password:
         assert char in valid_characters
 
